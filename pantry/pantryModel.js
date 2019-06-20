89b550b18a805058fd9353a266c74da7f233d0da
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-20 11:02:19 -0500

    Webtesting-iii-challenge Added test for insert and model in test
    Testing the model with test

---
+++ pantry/pantryModel.js
@@ -0,0 +1,33 @@
+const db = require("../data/dbconfig.js");
+
+module.exports = {
+  get,
+  getById,
+  modify,
+  remove,
+  add
+};
+
+function get() {
+  return null;
+}
+
+function getById() {
+  return null;
+}
+
+function modify() {
+  return null;
+}
+
+function remove() {
+  return null;
+}
+
+async function add(pantryItem) {
+  const [id] = await db("pantry").insert(pantryItem);
+
+  return db("pantry")
+    .where({ id })
+    .first();
+}
--- pantry/pantryModel.test.js
+++ pantry/pantryModel.test.js
@@ -1,7 +1,19 @@
-const req = require("supertest");
+const PantryModel = require("./pantryModel");
+const db = require("../data/dbconfig");
 
 describe("TS2::Testing PantryModel.js", () => {
-  it("TC5: Test get /pantry to return all pantry items", async () => {
-    console.log("Testing pantry model");
+  beforeEach(async () => {
+    await db("pantry").truncate();
+  });
+  describe("TS2.1:: Test Insert", () => {
+    it("TC5: Test get /pantry to return all pantry items", async () => {
+      const res = await PantryModel.add({
+        item_name: "Sugar",
+        item_description: "Grocery item",
+        item_quantity: 2
+      });
+      const pantryItems = await db("pantry");
+      expect(pantryItems).toHaveLength(1);
+    });
   });
 });
