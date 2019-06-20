5938a9230bf14462bff7b3bb0c144bed2188d52f
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-20 12:02:16 -0500

    Webteting-iv-challenge Completed the TC for insert
    Completed test for insert. Added one test for get

--- pantry/pantryModel.js
+++ pantry/pantryModel.js
@@ -9,7 +9,7 @@ module.exports = {
 };
 
 function get() {
-  return null;
+  return db("pantry");
 }
 
 function getById() {
--- pantry/pantryModel.test.js
+++ pantry/pantryModel.test.js
@@ -6,14 +6,42 @@ describe("TS2::Testing PantryModel.js", () => {
     await db("pantry").truncate();
   });
   describe("TS2.1:: Test Insert", () => {
-    it("TC5: Test get /pantry to return all pantry items", async () => {
-      const res = await PantryModel.add({
+    it("TC5: Positive - Test for inserting records", async () => {
+      await PantryModel.add({
         item_name: "Sugar",
         item_description: "Grocery item",
         item_quantity: 2
       });
       const pantryItems = await db("pantry");
       expect(pantryItems).toHaveLength(1);
+    });
+
+    it("TC6: Positive - Should add multiple records", async () => {
+      await PantryModel.add({
+        item_name: "Sugar",
+        item_description: "Grocery item",
+        item_quantity: 2
+      });
+      await PantryModel.add({
+        item_name: "Salt",
+        item_description: "Grocery item",
+        item_quantity: 1
+      });
+      const pantryItems = await db("pantry");
+      expect(pantryItems).toHaveLength(2);
+    });
+  });
+
+  describe("TS2.2:: Test Get()", () => {
+    it("TC5: Positive - Test for retrieving records", async () => {
+      await PantryModel.add({
+        item_name: "Sugar",
+        item_description: "Grocery item",
+        item_quantity: 2
+      });
+      const pantryItems = await db("pantry");
+      console.log(pantryItems);
+      expect(pantryItems[0].item_name).toBe("Sugar");
     });
   });
 });
