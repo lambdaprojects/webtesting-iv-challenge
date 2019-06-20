0a7e4e771a02a76f1f4387b719e871ddbdcf27ef
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-20 08:23:01 -0500

    Webtesting-iv-challenge Created migration and database
    created database for test and pantry

--- data/migrations/20190620081255_pantry.js
+++ data/migrations/20190620081255_pantry.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("pantry", tbl => {
+    tbl.increments();
+    tbl
+      .string("item_name", 255)
+      .notNullable()
+      .unique();
+    tbl.string("item_description", 255).notNullable();
+    tbl
+      .integer("item_quantity")
+      .notNullable()
+      .unsigned();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("pantry");
 };
